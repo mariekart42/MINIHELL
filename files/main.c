614c041c21f2683f5bb8865889801ec7db8d1778
mariekart42 <schwokelbaer@gmail.com>
2023-01-25 21:53:50 +0100

    whatever

--- files/main.c
+++ files/main.c
@@ -5,17 +5,26 @@
 int main(int32_t argc, char **argv)
 {
 	char *line = NULL;
+
+	// using signal function here to catch signal if eg ctr-c is used
+
+
 argc++;
 argv++;
 	while (1)
 	{
 		// read content from terminal
 		line = readline("MINIHELL> ");
-		parse_and_execute(line);
-		printf("content: %s\n", line);
+		if (!line)
+			break ;
+		printf("content before history: %s\n", line);
+		if (line && *line)	// if line exist and is not empty, stuff gets saved in history list
+		add_history(line);
+		// parse_and_execute(line);
+		printf("content after history: %s\n", line);
 		free(line);
-		break ;
 		
 	}
-	free(line);
+	//free(line);
 }
+
