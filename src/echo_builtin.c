e53cebcc872335fc372d0954ce6cbe454b51f54c
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-04 22:57:42 +0100

    Norminette

--- src/echo_builtin.c
+++ src/echo_builtin.c
@@ -29,6 +29,22 @@ bool	echo_builtin_helper(t_parsed_chunk *parsed_node, int i, bool is_nflag)
 	return (is_nflag);
 }
 
+void	echo_builtin_cont(t_parsed_chunk *parsed_node, int i, bool is_nflag)
+{
+	char	**args;
+
+	args = parsed_node->args;
+	while (args[i] != NULL)
+	{
+		ft_putstr_fd(args[i], parsed_node->outfile);
+		if (args[i + 1] != NULL)
+			ft_putstr_fd(" ", parsed_node->outfile);
+		i++;
+	}
+	if (!is_nflag)
+		ft_putstr_fd("\n", 1);
+}
+
 void	echo_builtin(t_parsed_chunk *parsed_node)
 {
 	char	**args;
@@ -52,14 +68,5 @@ void	echo_builtin(t_parsed_chunk *parsed_node)
 		is_nflag = temp_flag;
 		i++;
 	}
-	while (args[i] != NULL)
-	{
-		ft_putstr_fd(args[i], parsed_node->outfile);
-		if (args[i + 1] != NULL)
-			ft_putstr_fd(" ", parsed_node->outfile);
-		i++;
-	}
-	if (!is_nflag)
-		ft_putstr_fd("\n", 1);
-	return ;
+	echo_builtin_cont(parsed_node, i, is_nflag);
 }
