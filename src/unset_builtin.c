f3dff998de58fd29f721139b926098ed2d1c5356
stena-he <stena-he@student.42wolfsburg.de>
2023-03-06 20:43:14 +0100

    Finish draft of file

--- src/unset_builtin.c
+++ src/unset_builtin.c
@@ -1,39 +1,71 @@
-// #include "minishell.h"
+#include "minishell.h"
 
-// // https://man7.org/linux/man-pages/man1/unset.1p.html
+// https://man7.org/linux/man-pages/man1/unset.1p.html
 
-// void	delete_var (t_hold)
+void	delete_var (t_hold *hold, char *var) 
+{
+	t_env_export	*tmp;
+	t_env_export	*prev;
 
-// int	find_var(t_hold *hold, char *var)
+	tmp = hold->env_list;
+	if (tmp != NULL && (ft_strncmp(tmp->item, var, ft_strlen(var)) == 0))
+	{
+		hold->env_list = tmp->next;
+		free(tmp);
+		return ;
+	}
+	while (tmp != NULL && (ft_strncmp(tmp->item, var, ft_strlen(var)) != 0))
+	{
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	if (tmp == NULL)
+		return;
+	prev->next = tmp->next;
+	free(tmp);
+}
+
+bool	find_var(t_hold *hold, char *var)
+{
+	t_env_export	*tmp;
+	bool			var_exist;
+
+	var_exist = false;
+	tmp = hold->env_list;
+	while (tmp != NULL)
+	{
+		if (ft_strncmp(tmp->item, var, ft_strlen(var)) == 0)
+		{
+			var_exist = true;
+			delete_var(hold, var);
+		}
+		tmp = tmp->next;
+	}
+	free(tmp);
+	return (var_exist);
+}
+
+
+void unset_builtin(t_hold *hold, t_parsed_chunk *parsed_node)
+{
+	char	**args;
+	int		i;
+
+	args = parsed_node->args;
+	i = 1;
+	while (args[i] != NULL)
+	{
+		if (find_var(hold, args[i]) == false)
+		{
+			ft_putstr_fd(RED"minshell: unset: ", 2);
+			ft_putstr_fd(args[i], 2);
+			exit_status(hold, ": not a valid identifier\n"RESET, 69);
+			return ;
+		}
+		i++;
+	}
+}
+
+// void export_builtin(t_hold *hold)
 // {
-
-
-// 	return (0);
 // }
-
-
-// void unset_builtin(t_hold *hold, t_parsed_chunk *parsed_node)
-// {
-// 	char	**args;
-// 	int		i;
-
-// 	args = parsed_node->args;
-// 	i = 1;
-// 	while (args[i] != NULL)
-// 	{
-// 		if (find_var(hold, args[i]) == -1)
-// 		{
-// 			ft_putstr_fd(RED"minshell: unset: ", 2);
-// 			ft_putstr_fd(args[i], 2);
-// 			exit_status(hold, ": not a valid identifier\n"RESET, 69);
-// 			return ;
-// 		}
-// 		else
-			
-// 		i++
-// 	}
-// }
-
-// // void export_builtin(t_hold *hold)
-// // {
-// // }
