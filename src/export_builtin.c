9307aed18dda7db453ef0051228365640170d13a
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-07 23:14:52 +0100

    Expand logic

--- src/export_builtin.c
+++ src/export_builtin.c
@@ -16,12 +16,44 @@ void print_env_export(t_hold *hold)
 
 void export_builtin(t_hold *hold, t_parsed_chunk *parsed_node)
 {
+	int	i;
+	int	j;
+	int	var_class;
+
+	i = 0;
+	j = 0;
 	if (parsed_node->args[1] == NULL)
 		print_env_export(hold);
-	//while (args)
-		// if var
-		// if var=
-		// if var=value
+
+	while (parsed_node->args[i] != NULL)
+	{
+		var_class = 0;
+		while (parsed_node->args[i][j] != NULL)
+		{
+			if (parsed_node->args[i][j] == '=')
+			{
+				if (parsed_node->args[i][j + 1] == NULL)
+					var_class = 1;
+				else
+					var_class = 2;
+			}
+			j++;
+		}
+
+		// if var_class == 2
+			// ft_strjoin remaining args
+			// function(parsed_node->args[i])
+			// return
+
+		// if var_class = 1
+			// ft_strjoin ""
+			// function(parsed_node->args[i]);
+
+		// if var_class = 0
+			//function(parsed_node->args[i]);
+		j = 0;
+		i++;
+	}
 
 	// Modify env and export in both cases
 }
