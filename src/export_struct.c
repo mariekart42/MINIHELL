0d60785c6c42997fce53d72aaff29eaef31cc8ee
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-28 20:20:12 +0200

    Start norm

--- src/export_struct.c
+++ src/export_struct.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-void swap_data(t_env_exp *export_list)
+void	swap_data(t_env_exp *export_list)
 {
 	char	*tmp_item;
 	char	*tmp_value;
@@ -9,32 +9,25 @@ void swap_data(t_env_exp *export_list)
 	tmp_name = ft_strdup(export_list->var_name);
 	tmp_item = export_list->item;
 	tmp_value = ft_strdup(export_list->var_value);
-		free(export_list->var_name); 
-		free(export_list->var_value); 
-		// free(export_list->item); 
-
+	free(export_list->var_name);
+	free(export_list->var_value);
 	export_list->var_name = ft_strdup(export_list->next->var_name);
 	export_list->item = export_list->next->item;
 	export_list->var_value = ft_strdup(export_list->next->var_value);
 	free(export_list->next->var_name);
 	free(export_list->next->var_value);
-	// free(export_list->next->item);
-
 	export_list->next->var_name = tmp_name;
 	export_list->next->item = tmp_item;
 	export_list->next->var_value = tmp_value;
-	// free(tmp_item);
-	// free(tmp_name);
-	// free(tmp_value);
 }
 
-void sort_export_list(t_hold *hold)
+void	sort_export_list(t_hold *hold)
 {
-	// init var name and value in struct
-	t_env_exp *tmp;
-	char **tmp_tmp;
+	t_env_exp	*tmp;
+	char		**tmp_tmp;
+	int			i;
 
-	int i = 2;
+	i = 2;
 	tmp = hold->export_list;
 	while (tmp != NULL)
 	{
@@ -53,7 +46,9 @@ void sort_export_list(t_hold *hold)
 	tmp = hold->export_list;
 	while (hold->export_list != NULL && hold->export_list->next != NULL)
 	{
-		if (ft_strncmp(hold->export_list->var_name, hold->export_list->next->var_name, ft_strlen(hold->export_list->next->var_name)) > 0)
+		if (ft_strncmp(hold->export_list->var_name,
+				hold->export_list->next->var_name,
+				ft_strlen(hold->export_list->next->var_name)) > 0)
 		{
 			swap_data(hold->export_list);
 			hold->export_list = tmp;
@@ -64,11 +59,11 @@ void sort_export_list(t_hold *hold)
 	hold->export_list = tmp;
 }
 
-t_env_exp *new_node_env(void)
+t_env_exp	*new_node_env(void)
 {
-	t_env_exp *tmp;
+	t_env_exp	*tmp;
 
-	tmp = (t_env_exp*)malloc(sizeof(t_env_exp));
+	tmp = (t_env_exp *)malloc(sizeof(t_env_exp));
 	tmp->next = NULL;
 	tmp->item = NULL;
 	tmp->var_name = NULL;
@@ -76,10 +71,11 @@ t_env_exp *new_node_env(void)
 	return (tmp);
 }
 
-void add_node_env(t_hold *hold, char *content, char *type)
+void	add_node_env(t_hold *hold, char *content, char *type)
 {
-	t_env_exp *tmp;
-	t_env_exp *p;
+	t_env_exp	*tmp;
+	t_env_exp	*p;
+
 	tmp = new_node_env();
 	tmp->item = content;
 	if (ft_strncmp(type, "env", 3) == 0)
@@ -89,11 +85,11 @@ void add_node_env(t_hold *hold, char *content, char *type)
 		else
 		{
 			p = hold->env_list;
-			while(p->next != NULL)
+			while (p->next != NULL)
 				p = p->next;
 			p->next = tmp;
 		}
-		return;
+		return ;
 	}
 	else
 	{
@@ -106,7 +102,6 @@ void add_node_env(t_hold *hold, char *content, char *type)
 				p = p->next;
 			p->next = tmp;
 		}
-		return;
+		return ;
 	}
 }
-
