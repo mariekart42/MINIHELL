5cded9eb025c0b27922f3714801388652ffb9960
stena-he <stena-he@student.42wolfsburg.de>
2023-03-29 19:48:50 +0200

    Norm

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 19:40:18 by mmensing          #+#    #+#             */
-/*   Updated: 2023/03/27 20:23:31 by stena-he         ###   ########.fr       */
+/*   Updated: 2023/03/29 19:45:57 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ size_t	ft_strlen(const char *str)
 	int	i;
 
 	i = 0;
-	if(!str)
-		return 0;
+	if (!str)
+		return (0);
 	while (str[i] != '\0')
 		i++;
 	return (i);
