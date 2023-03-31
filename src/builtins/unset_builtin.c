5fd2e4362e6652af79851a9aca8b20c3e2db2e66
stena-he <stena-he@student.42wolfsburg.de>
2023-03-31 21:19:43 +0200

    Add commented func

--- src/builtins/unset_builtin.c
+++ src/builtins/unset_builtin.c
@@ -46,6 +46,33 @@ bool	find_var(t_hold *hold, char *var, char *structure)
 	return (var_exist);
 }
 
+// bool	find_var(t_hold *hold, char *var, char *structure)
+// {
+// 	t_env_exp	*tmp;
+// 	t_env_exp	*tmp_swap;
+// 	bool		var_exist;
+
+// 	var_exist = false;
+// 	if (ft_strncmp(structure, "env", 3) == 0)
+// 		tmp = hold->env_list;
+// 	if (ft_strncmp(structure, "export", 6) == 0)
+// 		tmp = hold->export_list;
+// 	while (tmp != NULL)
+// 	{
+// 		if (ft_strncmp(tmp->item, var, ft_strlen(var)) == 0)
+// 		{
+// 			var_exist = true;
+// 			tmp_swap->next = tmp->next;
+// 			free(tmp->item);
+// 			free(tmp);
+// 			// delete_var(&hold, var, structure);
+// 		}
+// 		tmp_swap = tmp; 
+// 		tmp = tmp->next;
+// 	}
+// 	return (var_exist);
+// }
+
 bool	first_char_is_num(t_pars *parsed_node, int i)
 {
 	if (ft_isdigit(parsed_node->args[i][0]) != 0)
